b51881ea7b400d9ce2f694662622008bc130f756
Khaimov <khaimov@reksoft.ru>
2023-11-28 11:23:54 +0300

    Added antd

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import styled from 'styled-components';
+
+import { DatePicker } from 'antd';
 
 const div = document.createElement('div');
 
@@ -8,23 +9,5 @@ div.setAttribute('id', 'root');
 
 document.body.appendChild(div);
 
-const Button = styled.a`
-  --accent-color: white;
-
-  /* This renders the buttons above... Edit me! */
-  background: transparent;
-  border-radius: 3px;
-  border: 1px solid var(--accent-color);
-  color: var(--accent-color);
-  display: inline-block;
-  margin: 0.5rem 1rem;
-  padding: 0.5rem 0;
-  transition: all 200ms ease-in-out;
-  width: 11rem;
-
-  &:hover {
-    filter: brightness(0.85);
-  }
-`;
-
-ReactDOM.createRoot(div).render(<Button>CLICK</Button>);
+console.log(12220202);
+ReactDOM.createRoot(div).render(<DatePicker />);
