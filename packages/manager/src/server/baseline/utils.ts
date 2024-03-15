04f03c945de7614a673d725f2aa469b2ded5fa8a
Komarov Aleksey <aykomarov@reksoft.ru>
2024-03-15 11:57:52 +0300

    Create directories based on presets

--- packages/manager/src/server/baseline/utils.ts
+++ packages/manager/src/server/baseline/utils.ts
@@ -1,10 +1,4 @@
 import fs from 'fs';
-import {
-  isNil,
-  ScreenshotName,
-  SelectedPresets,
-  StoryID,
-} from '@storyshots/core';
 
 export function exists(path: string): Promise<boolean> {
   return new Promise<boolean>((resolve) => fs.exists(path, resolve));
@@ -28,18 +22,4 @@ export function read(path: string): Promise<Buffer> {
 
 export function copy(curr: string, next: string): Promise<void> {
   return new Promise((resolve) => fs.copyFile(curr, next, () => resolve()));
-}
-
-export function constructScreenshotFileName(
-  id: StoryID,
-  name: ScreenshotName | undefined,
-  presets: SelectedPresets,
-): string {
-  const presetsName = Object.entries(presets ?? {})
-    .map((entry) => entry.join('-'))
-    .join('_');
-
-  return isNil(name)
-    ? `${id}_${presetsName}.png`
-    : `${id}_${presetsName}_${name}.png`;
 }
