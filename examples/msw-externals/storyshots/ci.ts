6d7bdaaa15ecf59238e0e372b0d7d6be885f12d9
khaimov <khaimov@reksoft.ru>
2025-03-05 18:22:53 +0300

    fix

--- examples/msw-externals/storyshots/ci.ts
+++ examples/msw-externals/storyshots/ci.ts
@@ -1,4 +1,4 @@
-import { runUI } from '@storyshots/manager';
+import { runInBackground } from '@storyshots/manager';
 import { config } from './config';
 
-void runUI(config);
+void runInBackground(config);
