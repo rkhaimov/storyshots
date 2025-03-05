a17d3c8e00b5a48e5b5ebeca0f089d44b01749fd
khaimov <khaimov@reksoft.ru>
2025-03-05 18:19:47 +0300

    add step

---
+++ examples/msw-externals/storyshots/config.ts
@@ -0,0 +1,31 @@
+import { mergeServe, runUI } from '@storyshots/manager';
+import { createWorkerSupplier } from '@storyshots/msw-externals/createWorkerSupplier';
+import path from 'path';
+import { createPreviewServe } from './createPreviewServe';
+
+export const config: Parameters<typeof runUI>[0] = {
+  devices: [
+    {
+      type: 'size-only',
+      name: 'desktop',
+      config: { width: 1480, height: 920 },
+    },
+    {
+      type: 'emulated',
+      name: 'mobile',
+      config: {
+        userAgent:
+          'Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1',
+        width: 414,
+        height: 896,
+        deviceScaleFactor: 3,
+      },
+    },
+  ],
+  paths: {
+    screenshots: path.join(process.cwd(), 'screenshots'),
+    records: path.join(process.cwd(), 'records'),
+    temp: path.join(process.cwd(), '..', '..', '..', 'temp'),
+  },
+  preview: mergeServe(createWorkerSupplier(), createPreviewServe()),
+};
